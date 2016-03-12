a9c5cefc2991435284f9401d7f8d17d7c92d304b
Aleksey Pak <aleksey.v.pak@gmail.com>
2016-03-12 16:00:00 +0300

    Customer lookup moved to a method in CustomerDAOTest.

--- LPTelecom/src/data/CustomerDAOTest.java
+++ LPTelecom/src/data/CustomerDAOTest.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 public class CustomerDAOTest {
 	public static void main(String[] args) {
 		CustomerDAO customerDAO = new CustomerDAO();
+		testJohn(customerDAO);
+	}
+
+	public static void testJohn(CustomerDAO customerDAO) {
 		try {
 			Customer customer = customerDAO.getCustomer("john@foo.com");
 			System.out.println(customer.getName());
