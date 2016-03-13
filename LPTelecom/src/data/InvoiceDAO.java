4aea7d6a02c7399db486ea15614cfdefef16935e
Aleksey Pak <aleksey.v.pak@gmail.com>
2016-03-14 02:15:45 +0300

    InvoiceDAO received a getInvoicesForCustomer method.

--- LPTelecom/src/data/InvoiceDAO.java
+++ LPTelecom/src/data/InvoiceDAO.java
@@ -18,7 +18,8 @@ public class InvoiceDAO {
 		ResultSet rs = statement.executeQuery();
 		List<Invoice> invoiceList = new LinkedList<>();
 		while (rs.next()) {
-			// String password = rs.getString("password");
+			// TODO: Some not so nice code duplication here. Has to be a better
+			// way.
 			int id = rs.getInt("service_id");
 			String service_name = rs.getString("service_name");
 			String description = rs.getString("descr");
@@ -34,8 +35,44 @@ public class InvoiceDAO {
 			boolean payed = rs.getBoolean("payed");
 			String invoice_status = rs.getString("invoice_status");
 			invoiceList.add(new Invoice(customer, newService, payed, invoice_status));
-			// serviceList.add(newService);
-			// customer = new Customer(email, password, name);
+		}
+		return invoiceList;
+
+	}
+
+	public static List<Invoice> getInvoicesForCustomer(Customer customer) throws SQLException {
+		return getInvoicesForEmail(customer.getEmail());
+	}
+
+	public static List<Invoice> getInvoicesForEmail(String email) throws SQLException {
+		String query = "SELECT * FROM customers JOIN invoices JOIN telecom_services "
+				+ "ON customers.customer_id = invoices.customer_id AND invoices.service_id = telecom_services.service_id "
+				+ "WHERE email = ? ";
+		Connection connection = ConnectionProviderMockup.getConnection();
+
+		PreparedStatement statement = connection.prepareStatement(query);
+
+		statement.setString(1, email);
+		ResultSet rs = statement.executeQuery();
+		List<Invoice> invoiceList = new LinkedList<>();
+		while (rs.next()) {
+			// TODO: Some not so nice code duplication here. Has to be a better
+			// way.
+			int id = rs.getInt("service_id");
+			String service_name = rs.getString("service_name");
+			String description = rs.getString("descr");
+			String service_name_alt = rs.getString("service_name_alt");
+			String description_alt = rs.getString("descr_alt");
+			TelecomService newService = new TelecomService(id, service_name, service_name_alt, description,
+					description_alt);
+			// String email = rs.getString("email");
+			String password = rs.getString("password");
+			String name = rs.getString("name");
+			String status = rs.getString("status");
+			Customer customer = new Customer(email, password, name, status);
+			boolean payed = rs.getBoolean("payed");
+			String invoice_status = rs.getString("invoice_status");
+			invoiceList.add(new Invoice(customer, newService, payed, invoice_status));
 		}
 		return invoiceList;
 
--- LPTelecom/src/data/InvoiceDAOTest.java
+++ LPTelecom/src/data/InvoiceDAOTest.java
@@ -1,10 +1,23 @@
 package data;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class InvoiceDAOTest {
 	public static void main(String[] args) {
 		listGetterTest();
+		System.out.println();
+		listGetterWithEmailTest("a@a.com");
+		CustomerDAO customerDAO = new CustomerDAO();
+		Customer john;
+		try {
+			john = customerDAO.getCustomer("john@foo.com");
+			listGetterWithCustomerTest(john);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void listGetterTest() {
@@ -12,7 +25,9 @@ public class InvoiceDAOTest {
 		// clash with a pre-rolled test framework.
 		System.out.println("Let's try to get all the invoices...");
 		try {
-			for (Invoice invoice : InvoiceDAO.getAllInvoices()) {
+			List<Invoice> allInvoices = InvoiceDAO.getAllInvoices();
+			System.out.printf("There are %d of them.\n", allInvoices.size());
+			for (Invoice invoice : allInvoices) {
 				System.out.printf("%s: %s: %s\n", invoice.getInvoiceCustomer().getName(),
 						invoice.getInvoiceTelecomService().getName(), invoice.getStatus());
 
@@ -21,5 +36,37 @@ public class InvoiceDAOTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static void listGetterWithEmailTest(String email) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's try to get all the invoices for " + email + "...");
+		try {
+			for (Invoice invoice : InvoiceDAO.getInvoicesForEmail(email)) {
+				System.out.printf("%s: %s: %s\n", invoice.getInvoiceCustomer().getName(),
+						invoice.getInvoiceTelecomService().getName(), invoice.getStatus());
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void listGetterWithCustomerTest(Customer customer) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's try to get all the invoices for a customer: " + customer.getName() + "...");
+		try {
+			for (Invoice invoice : InvoiceDAO.getInvoicesForCustomer(customer)) {
+				System.out.printf("%s: %s: %s\n", invoice.getInvoiceCustomer().getName(),
+						invoice.getInvoiceTelecomService().getName(), invoice.getStatus());
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
