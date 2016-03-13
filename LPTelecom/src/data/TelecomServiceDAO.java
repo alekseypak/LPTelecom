3e38f65fffffdca00f3f50ae1c5bd2d567c3330c
Aleksey Pak <aleksey.v.pak@gmail.com>
2016-03-13 17:20:37 +0300

    * TelecomServiceDAO has a sensible getAllTelecomServices method. * TelecomServiceDAOTest is modified accordingly.

--- LPTelecom/src/data/TelecomServiceDAO.java
+++ LPTelecom/src/data/TelecomServiceDAO.java
@@ -4,22 +4,30 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TelecomServiceDAO {
 
-	public static void getAllTelecomServices() throws SQLException {
+	public static List<TelecomService> getAllTelecomServices() throws SQLException {
 		String query = "SELECT * FROM telecom_services";
+
 		Connection connection = ConnectionProviderMockup.getConnection();
 		PreparedStatement statement = connection.prepareStatement(query);
 		ResultSet rs = statement.executeQuery();
+		List<TelecomService> serviceList = new LinkedList<>();
 		while (rs.next()) {
 			// String password = rs.getString("password");
+
 			String name = rs.getString("name");
 			String description = rs.getString("descr");
-			System.out.printf("%s: %s\n", name, description);
-
+			String name_alt = rs.getString("name_alt");
+			String description_alt = rs.getString("descr_alt");
+			TelecomService newService = new TelecomService(name, name_alt, description, description_alt);
+			serviceList.add(newService);
 			// customer = new Customer(email, password, name);
 		}
+		return serviceList;
 		// return customer;
 		// return null;
 	}
--- LPTelecom/src/data/TelecomServiceDAOTest.java
+++ LPTelecom/src/data/TelecomServiceDAOTest.java
@@ -4,8 +4,19 @@ import java.sql.SQLException;
 
 public class TelecomServiceDAOTest {
 	public static void main(String[] args) {
+		listGetterTest();
+	}
+
+	private static void listGetterTest() {
+		// Why is this not called getAllTelecomServicesTest? Perhaps it might
+		// clash with a pre-rolled test framework.
+		System.out.println("Let's try to get all the services...");
 		try {
-			TelecomServiceDAO.getAllTelecomServices();
+			for (TelecomService ts : TelecomServiceDAO.getAllTelecomServices()) {
+				System.out.printf("%s: %s\n", ts.getName(), ts.getDescr());
+
+			}
+			;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
