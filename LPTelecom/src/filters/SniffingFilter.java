c37b4cb18341a4206597171accf18b68341f9023
Aleksey Pak <aleksey.v.pak@gmail.com>
2016-03-16 21:15:20 +0300

    A Filter example added: SniffingFilter.

---
+++ LPTelecom/src/filters/SniffingFilter.java
@@ -0,0 +1,60 @@
+package filters;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class SniffingFilter
+ */
+@WebFilter("/ControllerLogin")
+public class SniffingFilter implements Filter {
+
+	private static final Logger LOGGER = Logger.getLogger(SniffingFilter.class.getName());
+
+	/**
+	 * Default constructor.
+	 */
+	public SniffingFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		LOGGER.info("Login attempt: " + email + ":" + password + ", tell no one.");
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
